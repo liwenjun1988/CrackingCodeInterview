aba5c33b5c11480c4cf4e737994116235c28e28e
Wenjun Li <vickyleeadvance@gmail.com>
2018-01-04 22:37:45 -0500

    Update 2.1RemoveDups.java

--- c2/2.1RemoveDups.java
+++ c2/2.1RemoveDups.java
@@ -1,12 +1,32 @@
 import java.util.HashSet;
 import helpers.LinkedListNode;
 
-public void removeDups(LinkedListNode n){
+//with extra space
+public void removeDups1(LinkedListNode n){
     Hashset<Integer> set = new Hashset<Integer>();
     LinkedListNode previous = null;
     while(n != null){
         if(set.contains(n.data)){
             previous.next = n.next;
-        }
+        }else{
+            set.add(n.data);
+            previous = n;
+        }                
+        n = n.next;
     }
 } 
+
+//without extra space
+public void removeDups2(LinkedlistNode n){
+    while(n != null){
+        LinkedListNode runner = n;
+        while(runner.next != null){
+            if(runner.next.data == n.data){
+                runner.next = runner.next.next;
+            }else{
+                runner = runner.next;
+            }
+        }
+        n = n.next;
+    }
+}
